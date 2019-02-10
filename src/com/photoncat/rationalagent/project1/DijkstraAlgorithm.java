027e12458def5873e74c38642a5b0577a68cf72d
Photon Cat <lkstc112233@gmail.com>
2019-02-09 20:28:17 -0500

    Implemented Dijkstra's algorithm

---
+++ src/com/photoncat/rationalagent/project1/DijkstraAlgorithm.java
@@ -0,0 +1,36 @@
+package com.photoncat.rationalagent.project1;
+
+import com.photoncat.rationalagent.project1.util.LoadedGraph;
+import com.photoncat.rationalagent.project1.util.OnetimePriorityQueue;
+
+import java.util.*;
+
+/**
+ * A class that holds the algorithm as in Dijkstra's algorithm.
+ */
+public class DijkstraAlgorithm {
+    private LoadedGraph graph;
+
+    public DijkstraAlgorithm(LoadedGraph graph) {
+        this.graph = graph;
+    }
+
+    public int shortestPath(int start, int end) {
+        OnetimePriorityQueue<LoadedGraph.GraphNode> frienge = new OnetimePriorityQueue<LoadedGraph.GraphNode>();
+        frienge.add(graph.getNode(start), 0);
+        var goal = graph.getNode(end);
+        while (!frienge.isEmpty() && frienge.peek().getKey() != goal) {
+            var pair = frienge.poll();
+            var currentNode = pair.getKey();
+            int currentValue = pair.getValue();
+            for (var nodeNeighbour: currentNode) {
+                if (!frienge.extracted(nodeNeighbour))
+                    frienge.add(nodeNeighbour, currentValue + currentNode.distanceTo(nodeNeighbour));
+            }
+        }
+        if (frienge.isEmpty()) {
+            return -1;
+        }
+        return frienge.peek().getValue();
+    }
+}
