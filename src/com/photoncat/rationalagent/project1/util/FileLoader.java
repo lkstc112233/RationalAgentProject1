18f3afcfa2319fb2e5df733107cd9dab826809ed
Photon Cat <lkstc112233@gmail.com>
2019-02-09 19:43:13 -0500

    Fixed where the program is not initialized.

--- src/com/photoncat/rationalagent/project1/util/FileLoader.java
+++ src/com/photoncat/rationalagent/project1/util/FileLoader.java
@@ -56,6 +56,7 @@ public class FileLoader {
         while (true) {
             if (!parseOptionalEdge(tokens, program)) break;
         }
+        program.createEdgesCache();
 		return program;
 	}
     /**
