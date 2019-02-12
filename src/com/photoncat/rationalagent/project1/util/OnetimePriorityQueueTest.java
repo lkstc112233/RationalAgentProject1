90f2c1a390bd48522fb3cce6f9f16718e1da94d1
Photon Cat <lkstc112233@gmail.com>
2019-02-12 01:30:09 -0500

    Updated the test for the new behaviour.

--- src/com/photoncat/rationalagent/project1/util/OnetimePriorityQueueTest.java
+++ src/com/photoncat/rationalagent/project1/util/OnetimePriorityQueueTest.java
@@ -23,11 +23,17 @@ public class OnetimePriorityQueueTest {
         if (queue.poll().getKey() != 3) throw new AssertionError();
         if (!queue.extracted(3)) throw new AssertionError();
         if (queue.peek().getKey() != 1) throw new AssertionError();
-        // Polled element cannot be re-added.
+        // Polled element cannot be re-added with a higher key than when it's polled.
         if (queue.add(3, 3)) throw new AssertionError();
-        if (queue.add(3, 2)) throw new AssertionError();
         if (queue.add(3, 5)) throw new AssertionError();
         if (queue.peek().getKey() != 1) throw new AssertionError();
+        // Polled element can be re-added with a smaller key than when it's polled.
+        if (!queue.add(3, 2)) throw new AssertionError();
+        if (queue.peek().getKey() != 3) throw new AssertionError();
+        // When re-added the extracted is no longer true.
+        if (queue.extracted(3)) throw new AssertionError();
+        if (queue.poll().getKey() != 3) throw new AssertionError();
+        if (!queue.extracted(3)) throw new AssertionError();
         // Decreasing un-polled key
         if (!queue.add(2, 3)) throw new AssertionError();
         if (queue.peek().getKey() != 2) throw new AssertionError();
