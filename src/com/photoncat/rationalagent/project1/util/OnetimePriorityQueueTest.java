b757f0daab12a6e0f13b273663068866b26680f6
Photon Cat <lkstc112233@gmail.com>
2019-02-12 01:29:46 -0500

    Assertion is now if-statements.

--- src/com/photoncat/rationalagent/project1/util/OnetimePriorityQueueTest.java
+++ src/com/photoncat/rationalagent/project1/util/OnetimePriorityQueueTest.java
@@ -10,34 +10,34 @@ import java.util.PriorityQueue;
 public class OnetimePriorityQueueTest {
     public static void main(String[] args) {
         OnetimePriorityQueue<Integer> queue = new OnetimePriorityQueue<>();
-        assert queue.isEmpty();
+        if (!queue.isEmpty()) throw new AssertionError();
         // Add some elements
-        assert queue.add(1, 4);
-        assert queue.peek().getKey() == 1;
-        assert queue.add(2, 5);
-        assert queue.peek().getKey() == 1;
-        assert queue.add(3, 3);
-        assert queue.peek().getKey() == 3;
+        if (!queue.add(1, 4)) throw new AssertionError();
+        if (queue.peek().getKey() != 1) throw new AssertionError();
+        if (!queue.add(2, 5)) throw new AssertionError();
+        if (queue.peek().getKey() != 1) throw new AssertionError();
+        if (!queue.add(3, 3)) throw new AssertionError();
+        if (queue.peek().getKey() != 3) throw new AssertionError();
         // Examine poll.
-        assert !queue.extracted(3);
-        assert queue.poll().getKey() == 3;
-        assert queue.extracted(3);
-        assert queue.peek().getKey() == 1;
+        if (queue.extracted(3)) throw new AssertionError();
+        if (queue.poll().getKey() != 3) throw new AssertionError();
+        if (!queue.extracted(3)) throw new AssertionError();
+        if (queue.peek().getKey() != 1) throw new AssertionError();
         // Polled element cannot be re-added.
-        assert !queue.add(3, 3);
-        assert !queue.add(3, 2);
-        assert !queue.add(3, 5);
-        assert queue.peek().getKey() == 1;
+        if (queue.add(3, 3)) throw new AssertionError();
+        if (queue.add(3, 2)) throw new AssertionError();
+        if (queue.add(3, 5)) throw new AssertionError();
+        if (queue.peek().getKey() != 1) throw new AssertionError();
         // Decreasing un-polled key
-        assert queue.add(2, 3);
-        assert queue.peek().getKey() == 2;
-        assert !queue.extracted(2);
-        assert queue.poll().getKey() == 2;
-        assert queue.extracted(2);
-        assert !queue.isEmpty();
-        assert queue.poll().getKey() == 1;
-        assert queue.extracted(1);
-        assert queue.isEmpty();
+        if (!queue.add(2, 3)) throw new AssertionError();
+        if (queue.peek().getKey() != 2) throw new AssertionError();
+        if (queue.extracted(2)) throw new AssertionError();
+        if (queue.poll().getKey() != 2) throw new AssertionError();
+        if (!queue.extracted(2)) throw new AssertionError();
+        if (queue.isEmpty()) throw new AssertionError();
+        if (queue.poll().getKey() != 1) throw new AssertionError();
+        if (!queue.extracted(1)) throw new AssertionError();
+        if (!queue.isEmpty()) throw new AssertionError();
         System.out.println("All test passed.");
     }
 }
